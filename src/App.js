28010ef8a5d25ec5a77f4b9a83257fabe0e91660
Razan Harashe <harashe115@gmail.com>
2024-06-28 14:57:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 
-function App() {
+const Home = () => (
+  <div>
+    <h1>Welcome</h1>
+  </div>
+);
+
+const NumberDisplay = ({ number }) => (
+  <div>
+    <h1>The number is: {number}</h1>
+  </div>
+);
+
+const WordDisplay = ({ word, textColor, bgColor }) => {
+  const style = {
+    color: textColor,
+    backgroundColor: bgColor
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <h1>The word is: {word}</h1>
     </div>
   );
-}
+};
+
+const RouteHandler = () => {
+  const { param1, param2, param3 } = useParams();
+  
+  if (!isNaN(param1)) {
+    // If param1 is a number
+    return <NumberDisplay number={param1} />;
+  } else if (param2 && param3) {
+    // If param2 and param3 are they are colors
+    return <WordDisplay word={param1} textColor={param2} bgColor={param3} />;
+  } else {
+    // If param1 is a word
+    return <WordDisplay word={param1} />;
+  }
+};
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/:param1/:param2?/:param3?" element={<RouteHandler />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
